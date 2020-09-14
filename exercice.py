caeb0354f45998a20af0742ee36529b3c565142a
chibi20 <lea-aurore.pajzler@polymtl.ca>
2020-09-14 23:24:12 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -29,7 +29,7 @@ def to_degrees(angle_rads: float) -> tuple:
     angle = angle_rads*(180/math.pi)
     angle_degré = angle%60
     angle_minute = (angle-angle_degré)%60
-    angle_seconde = (angle_degré-angle_minute
+    angle_seconde = (angle - angle_degré-angle_minute
     return angle_degré, 0.0, 0.0
 
 
