ea1dc9e778ad96ebd4bf468f0b1300ace7776310
chibi20 <lea-aurore.pajzler@polymtl.ca>
2020-09-14 22:30:49 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -34,7 +34,7 @@ def to_celsius(temperature: float) -> float:
 
 
 def to_farenheit(temperature: float) -> float:
-    temperature_F = 32*temperature
+    temperature_F = temperature
     return 0.0
 
 
