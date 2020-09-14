d4ddeb0a14df004f5eb7d0eed43f07d18d22c352
chibi20 <lea-aurore.pajzler@polymtl.ca>
2020-09-14 22:42:58 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -20,8 +20,8 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    rad = angle_degs+(angle_mins+angle_secs/60)/60
-    
+    décimal = angle_degs+(angle_mins+angle_secs/60)/60
+
     return 0.0
 
 
